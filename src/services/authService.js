421f32ccaa5f985eaa0f35168c51c071c03b05f7
kaniela04 <karenguzmanhernandez44@gmail.com>
2025-09-16 14:57:40 -0500

    Devuelta a lo funcional

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,7 +1,9 @@
 // services/authService.js
 
-// URL base de tu backend
-const API_URL = "http://localhost:4000"; 
+
+// URL base de tu backend - usando variable de entorno
+const API_URL = import.meta.env.VITE_API_URL; 
+
 
 // ----------------- REGISTRO -----------------
 export async function registerUser(userData) {
